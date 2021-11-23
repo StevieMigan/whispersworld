8b1921f15d6f09eba829c57e258afea4dd79987a
Stevie Migan <stevie@radcub.net>
2021-11-23 17:52:02 +0000

    Add card array to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const cardArray = [
+
+    {
+        name: 'clover',
+        img: 'images/clover-card.png'
+      },
+      {
+        name: 'dragon-egg',
+        img: 'images/dragon-egg-card.png'
+      },
+      {
+        name: 'lightning',
+        img: 'images/lightning-card.png'
+      },
+      {
+        name: 'magic-potion',
+        img: 'images/magic-potion-card.png'
+      },
+      {
+        name: 'moon',
+        img: 'images/moon-card.png'
+      },
+      {
+        name: 'mushroom',
+        img: 'images/mushroom-card.png'
+      },
+      {
+        name: 'rainbow-star',
+        img: 'images/rainbow-star-card.png'
+      },
+      {
+        name: 'rose',
+        img: 'images/rose-card.png'
+      },
+      {
+        name: 'clover',
+        img: 'images/clover-card.png'
+      },
+      {
+        name: 'dragon-egg',
+        img: 'images/dragon-egg-card.png'
+      },
+      {
+        name: 'lightning',
+        img: 'images/lightning-card.png'
+      },
+      {
+        name: 'magic-potion',
+        img: 'images/magic-potion-card.png'
+      },
+      {
+        name: 'moon',
+        img: 'images/moon-card.png'
+      },
+      {
+        name: 'mushroom',
+        img: 'images/mushroom-card.png'
+      },
+      {
+        name: 'rainbow-star',
+        img: 'images/rainbow-star-card.png'
+      },
+      {
+        name: 'rose',
+        img: 'images/rose-card.png'
+      },
+    ]
+}
+
+
+
+
+
+
+
+}
