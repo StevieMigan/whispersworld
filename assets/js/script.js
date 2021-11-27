eea0e0ffcf83aaf68cc6b74825a42bf7b54ba938
Stevie Migan <stevie@radcub.net>
2021-11-27 19:43:07 +0000

    Add createGame function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,87 @@
+console.log("testing!")
+
 document.addEventListener('DOMContentLoaded', () => {
+
 
     const cardArray = [
 
         {
             name: 'clover',
-            img: 'images/clover-card.png'
+            img: 'assets/images/clover-card.png'
         },
         {
             name: 'dragon-egg',
-            img: 'images/dragon-egg-card.png'
+            img: 'assets/images/dragon-egg-card.png'
         },
         {
             name: 'lightning',
-            img: 'images/lightning-card.png'
+            img: 'assets/images/lightning-card.png'
         },
         {
             name: 'magic-potion',
-            img: 'images/magic-potion-card.png'
+            img: 'assets/images/magic-potion-card.png'
         },
         {
             name: 'moon',
-            img: 'images/moon-card.png'
+            img: 'assets/images/moon-card.png'
         },
         {
             name: 'mushroom',
-            img: 'images/mushroom-card.png'
+            img: 'assets/images/mushroom-card.png'
         },
         {
             name: 'rainbow-star',
-            img: 'images/rainbow-star-card.png'
+            img: 'assets/images/rainbow-star-card.png'
         },
         {
             name: 'rose',
-            img: 'images/rose-card.png'
+            img: 'assets/images/rose-card.png'
         },
         {
             name: 'clover',
-            img: 'images/clover-card.png'
+            img: 'assets/images/clover-card.png'
         },
         {
             name: 'dragon-egg',
-            img: 'images/dragon-egg-card.png'
+            img: 'assets/images/dragon-egg-card.png'
         },
         {
             name: 'lightning',
-            img: 'images/lightning-card.png'
+            img: 'assets/images/lightning-card.png'
         },
         {
             name: 'magic-potion',
-            img: 'images/magic-potion-card.png'
+            img: 'assets/images/magic-potion-card.png'
         },
         {
             name: 'moon',
-            img: 'images/moon-card.png'
+            img: 'assets/images/moon-card.png'
         },
         {
             name: 'mushroom',
-            img: 'images/mushroom-card.png'
+            img: 'assets/images/mushroom-card.png'
         },
         {
             name: 'rainbow-star',
-            img: 'images/rainbow-star-card.png'
+            img: 'assets/images/rainbow-star-card.png'
         },
         {
             name: 'rose',
-            img: 'images/rose-card.png'
+            img: 'assets/images/rose-card.png'
         }
     ]
 
-})
+    cardArray.sort(() => 0.5 - Math.random())
+
+  
+
+    function createBoard() {
+      for (let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'assets/images/cat-card.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        grid.appendChild(card)
+      }
+    }
+    })
